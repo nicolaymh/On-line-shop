f23b5f7912bdd10905802c0096b967a286065073
Nicolay Moreno <nicolay.mherrera@gmail.com>
2023-02-25 00:04:57 -0500

    designing the login

--- Frontend/src/components/LoginForm.jsx
+++ Frontend/src/components/LoginForm.jsx
@@ -1,6 +1,6 @@
 import { useForm } from "../Hooks/useForm";
 
-import logo from "../assets/logo.png";
+import logo from "../assets/logo-final.png";
 
 import style from "../sass/forms/login.module.scss";
 
@@ -13,51 +13,65 @@ const LoginForm = () => {
   const { email, password } = formState;
 
   return (
-    <>
-      <form>
-        <div>
-          <img src={logo} alt="logo" />
-          <div>
-            <h1 className={`${style.h1}`}>
-              Game{" "}
-              <span className={style.size}>
-                <h3 className={style.h3Color}>STORE</h3>
-              </span>
-            </h1>
-
-            <h1 className={style.size}>
-              Hola <span>Mundo!</span>
-            </h1>
+    <div className={`mainContainer ${style.container}`}>
+      <div className={style.loginBox}>
+        <div className={style.logoTitle}>
+          <div className={style.logo}>
+            <img src={logo} alt="logo" />
           </div>
+
+          <h1>
+            GAMER <span>STORE</span>
+          </h1>
         </div>
 
-        <div>
-          <label htmlFor="email">E-mail</label>
-          <input
-            id="email"
-            name="email"
-            type="email"
-            placeholder="Your e-mail"
-            autoComplete="off"
-            value={email}
-            onChange={onInputChange}
-          />
-        </div>
+        <form className={style.form}>
+          <div className={style.field}>
+            <label htmlFor="email">E-mail:</label>
+            <input
+              id="email"
+              name="email"
+              type="email"
+              placeholder="Your e-mail"
+              autoComplete="off"
+              value={email}
+              onChange={onInputChange}
+            />
+          </div>
 
-        <div>
-          <label htmlFor="password">Password</label>
-          <input
-            id="password"
-            name="password"
-            type="password"
-            placeholder="Your password"
-            autoComplete="off"
-            value={password}
-            onChange={onInputChange}
-          />
-        </div>
-      </form>
-    </>
+          <div className={style.field}>
+            <label htmlFor="password">Password:</label>
+            <input
+              id="password"
+              name="password"
+              type="password"
+              placeholder="Your password"
+              autoComplete="off"
+              value={password}
+              onChange={onInputChange}
+            />
+          </div>
+
+          <div className={style.field}>
+            <input type="submit" value="Log In" />
+          </div>
+
+          <div className={style.field}>
+            <div className={style.containerLink}>
+              <a className={style.link} href="#">
+                Create a New Account
+              </a>
+            </div>
+
+            <div className={style.containerLink}>
+              <a className={style.link} href="#">
+                Forgot Password?
+              </a>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
   );
 };
 
--- Frontend/src/layouts/AuthLayout.jsx
+++ Frontend/src/layouts/AuthLayout.jsx
@@ -3,9 +3,7 @@ import { Outlet } from "react-router-dom";
 const AuthLayout = () => {
   return (
     <>
-      <main className="container">
-        <Outlet />
-      </main>
+      <Outlet />
     </>
   );
 };
