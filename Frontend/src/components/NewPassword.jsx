ea07c33956beb5de67dac8ac6eb18e4076cdf1ce
Nicolay Moreno <nicolay.mherrera@gmail.com>
2023-03-06 10:08:36 -0500

    Finished NewPassword.jsx component design

--- Frontend/src/components/NewPassword.jsx
+++ Frontend/src/components/NewPassword.jsx
@@ -1,8 +1,72 @@
+import { Link } from "react-router-dom";
+import { useForm } from "../Hooks/useForm";
+
+// Assets
+import logo from "../assets/logo-final.png";
+import newPasswordImage from "../assets/images/new-password.png";
+
+// CSS styles ( SASS Modules )
+import styles from "../sass/forms/generalFormStyle.module.scss";
 
 const NewPassword = () => {
-  return (
-    <div>NewPassword</div>
-  )
-}
+  const { formState, onInputChange } = useForm({
+    newPassword: "",
+    confirmPassword: "",
+  });
 
-export default NewPassword
+  const { newPassword, confirmPassword } = formState;
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.loginBox}>
+        <div className={styles.logoTitle}>
+          <div className={styles.logo}>
+            <img src={logo} alt="logo" />
+          </div>
+
+          <h1>
+            GAMER <span>STORE</span>
+          </h1>
+        </div>
+
+        <form className={styles.form}>
+          <div className={styles.field}>
+            <label htmlFor="newPassword">New Password: </label>
+            <input
+              id="newPassword"
+              name="newPassword"
+              type="newPassword"
+              placeholder="Create a new password"
+              autoComplete="off"
+              value={newPassword}
+              onChange={onInputChange}
+            />
+          </div>
+
+          <div className={styles.field}>
+            <label htmlFor="confirmPassword">Confirm-password </label>
+            <input
+              id="confirmPassword"
+              name="confirmPassword"
+              type="confirmPassword"
+              placeholder="Confirm your password"
+              autoComplete="off"
+              value={confirmPassword}
+              onChange={onInputChange}
+            />
+          </div>
+
+          <div className={styles.field}>
+            <input type="submit" value="SAVE NEW PASSWORD" />
+          </div>
+        </form>
+      </div>
+
+      <div className={styles.loginImg}>
+        <img height={350} src={newPasswordImage} alt="register-image" />
+      </div>
+    </div>
+  );
+};
+
+export default NewPassword;
