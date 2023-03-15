6ad220ce6c6f868402294eecd218bf1d7a0243b5
Nicolay Moreno <nicolay.mherrera@gmail.com>
2023-03-15 12:21:39 -0500

    separate fronted path in environment variable in index.js (backend)

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -19,7 +19,7 @@ dotenv.config();
 dbConnection();
 
 // CORS configuration
-const whitelist = ["http://127.0.0.1:5173"];
+const whitelist = [process.env.FRONTEND_URL];
 const corsOptions = {
   origin: function (origin, callback) {
     if (whitelist.indexOf(origin) !== -1 || !origin) {
