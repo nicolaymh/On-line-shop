1f372448b915e5f0f77303f2b6e27d42c19321da
Nicolay Moreno <nicolay.mherrera@gmail.com>
2023-03-29 20:50:33 -0500

    Adding a spinner to load pages (frontend)

---
+++ Frontend/src/components/general-components/Spinner.jsx
@@ -0,0 +1,16 @@
+
+
+const Spinner = () => {
+  return (
+    <>
+      <h1>Loading Animation w/ Translate and Z-index</h1>
+      <div class="container">
+        <div class="circle"></div>
+        <div class="circle"></div>
+        <div class="circle"></div>
+      </div>
+    </>
+  );
+};
+
+export default Spinner;
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -4,9 +4,12 @@ import App from "./App";
 
 import "./sass/base/normalize.scss";
 import "./sass/base/global.scss";
+import "./sass/spinner/spinner.scss";
+
+import Spinner from "./components/general-components/Spinner";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Spinner />
   </React.StrictMode>
 );
