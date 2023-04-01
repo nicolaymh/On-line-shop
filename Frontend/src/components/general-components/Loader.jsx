e5ff2cd8724a18d0d5cc9596e6bbf7198c9030da
Nicolay Moreno <nicolay.mherrera@gmail.com>
2023-03-31 21:22:25 -0500

    starting to add animations

--- Frontend/src/components/general-components/Loader.jsx
+++ Frontend/src/components/general-components/Loader.jsx
@@ -1,7 +1,17 @@
+import style from "../../sass/screenLoader/loader.module.scss";
+
+import logoImage from "../../assets/logo-final.png";
+
 const Loader = () => {
   return (
-    <div>
-      <h1>Loader</h1>
+    <div className={`${style.logerContainer}`}>
+      <div className={style.imageContainer}>
+        <img src={logoImage} alt="logo-image" />
+      </div>
+
+      <div className={style.noticeContainer}>
+        <h1>Loading</h1>
+      </div>
     </div>
   );
 };
