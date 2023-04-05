cf70ef1bf62ea4e6a40b09d8cc15b56bf1bcbd60
Nicolay Moreno <nicolay.mherrera@gmail.com>
2023-04-04 19:19:45 -0500

    category.controller.js => Created createCategory function (backend)

--- Backend/src/controllers/category.controller.js
+++ Backend/src/controllers/category.controller.js
@@ -2,29 +2,29 @@ import internalServerError from "../helpers/internalServerError.js";
 import Category from "../models/CatagoryModel.js";
 
 const createCategory = async (req, res) => {
+  console.log(req.user);
+
   const { name } = req.body;
 
-  const nameExists = await Category.findOne({ name });
-
-  if (nameExists) {
-    return res.status(400).json({ ok: false, msg: `There is already a category called ${name}` });
-  }
+  const nameExits = await Category.findOne({ name });
 
   try {
+    if (nameExits) {
+      return res.status(400).json({ ok: false, msg: `There is already a category called ${name}` });
+    }
+
     const newCategory = new Category(req.body);
 
     await newCategory.save();
 
-    res.status(201).json({ ok: true, msg: `Created ${newCategory.name} category successfully` });
+    res.status(201).json({ ok: true, msg: "Category created successfully" });
   } catch (error) {
     console.log(error);
     internalServerError(error, res);
   }
 };
 
-const getCategoriesInfo = (req, res) => {
-  console.log("asdfas");
-};
+const getCategoriesInfo = async (req, res) => {};
 
 const updateCategory = async (req, res) => {};
 
--- Backend/src/routes/category.routes.js
+++ Backend/src/routes/category.routes.js
@@ -3,10 +3,11 @@ import { Router } from "express";
 import verifyCategoryFields from "../middlewares/validate-category.js";
 
 import * as categoryController from "../controllers/category.controller.js";
+import checkAuth from "../middlewares/checkAuth.js";
 
 const router = Router();
 
-router.post("/create", verifyCategoryFields, categoryController.createCategory);
+router.post("/create", checkAuth, verifyCategoryFields, categoryController.createCategory);
 router.get("/get-categories-info");
 router.put("/update-category", verifyCategoryFields, categoryController.updateCategory);
 
