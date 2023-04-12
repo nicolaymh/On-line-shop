99bc176ff75758f8904340a26eee4bf84d7c0ab2
Nicolay Moreno <nicolay.mherrera@gmail.com>
2023-04-11 20:14:50 -0500

    +++

--- Frontend/src/components/private-components/Header.jsx
+++ Frontend/src/components/private-components/Header.jsx
@@ -1,15 +1,41 @@
 import { NavLink } from "react-router-dom";
 
+// Assets
+import logo from "../../assets/logo-final.png";
+
+// Styles
+import style from "../../sass/Header/header.module.scss";
+
 const Header = () => {
   return (
-    <div>
-      <div>Logo</div>
+    <div className={style.headerContainer}>
+      <div className={style.containerLogo}>
+        <div className={style.containerImg}>
+          <img src={logo} alt="logo" />
+        </div>
 
-      <nav>
-        <NavLink to="/shop/">Home</NavLink>
-        <NavLink to="/shop/Categories">Categories</NavLink>
-        <NavLink to="/shop/settings">Settings</NavLink>
+        <h1>
+          GAMER <span>STORE</span>
+        </h1>
+      </div>
+
+      <nav className={style.containerNav}>
+        <NavLink className={style.navLink} to="/shop">
+          Home
+        </NavLink>
+
+        <NavLink className={style.navLink} to="/shop/Categories">
+          Categories
+        </NavLink>
+
+        <NavLink className={style.navLink} to="/shop/settings">
+          Settings
+        </NavLink>
       </nav>
+
+      <div className={style.containerUser}>
+        <h3>User</h3>
+      </div>
     </div>
   );
 };
