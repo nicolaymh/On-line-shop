ee6d001f113dbcfe678630886de3858cc8eceb0a
Nicolay Moreno <nicolay.mherrera@gmail.com>
2023-04-25 12:34:18 -0500

    starting to create initial setup categories => {backend}

--- Backend/src/helpers/initialSetup/CreateAdmin.js
+++ Backend/src/helpers/initialSetup/CreateAdmin.js
@@ -3,21 +3,15 @@ import bcrypt from "bcryptjs";
 import Role from "../../models/RoleModel.js";
 import User from "../../models/UserModel.js";
 
-const adminInfo = User({
+// Admin Info
+const adminInfo = {
   name: process.env.ADMIN_NAME,
   password: process.env.PASSWORD,
   email: process.env.EMAIL,
   address: process.env.ADDRESS,
   phone: process.env.PHONE,
-});
+};
 
-/**
- * This function creates an admin user with encrypted password and assigns admin role if the user does
- * not already exist.
- * @returns If `adminExists` is truthy, the function will return without doing anything. If
- * `adminExists` is falsy, the function will create a new admin user and save it to the database. No
- * explicit value is returned in this case.
- */
 const createAdmin = async () => {
   try {
     // Verify if admin exists
@@ -38,7 +32,8 @@ const createAdmin = async () => {
     admin.confirmed = true;
     admin.role = id;
 
-    await admin.save();
+    const adm = await admin.save();
+    console.log(adm);
   } catch (error) {
     console.log(error);
   }
