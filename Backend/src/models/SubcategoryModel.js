a315ad34b08a2e66be7bc59b3fbe22b8297eb8ce
Nicolay Moreno <nicolay.mherrera@gmail.com>
2023-04-25 19:24:57 -0500

    Created subcategory model => {backend}

---
+++ Backend/src/models/SubcategoryModel.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const subcategorySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      require: true,
+      trim: true,
+    },
+
+    description: {
+      type: String,
+      require: true,
+      trim: true,
+    },
+
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category",
+    },
+  },
+  { timestamps: true }
+);
+
+const Subcategory = mongoose.model("Subcategory", subcategorySchema);
+
+export default Subcategory;
