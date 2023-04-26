2684b53046b84c9c1f0f324fd4f33cd54ee3982b
Nicolay Moreno <nicolay.mherrera@gmail.com>
2023-04-25 22:40:41 -0500

    Restructuring createSubcategory() function => (backend)

--- Backend/src/controllers/subcategories.controller.js
+++ Backend/src/controllers/subcategories.controller.js
@@ -3,36 +3,45 @@ import Subcategory from "../models/SubcategoryModel.js";
 
 import internalServerError from "../helpers/internalServerError.js";
 
-const getSubcategories = async () => {
-  console.log("From getSubcategories controller");
-};
-
 const createSubcategory = async (req, res) => {
   try {
-    const { category } = req.params;
-    const { name } = req.body;
+    let { category } = req.params;
+    category = category.toLowerCase();
 
-    const subcategoryExists = await Subcategory.findOne({ name });
+    let { name } = req.body;
+    name = name.toLowerCase();
+
+    const searchCategory = await Category.findOne({ name: category });
+    if (!searchCategory) {
+      return res.status(400).json({ ok: false, msg: `There is no category called ${category}` });
+    }
+
+    const subcategoryExists = await Subcategory.findOne({
+      name,
+      category: searchCategory._id,
+    });
+
     if (subcategoryExists) {
-      return res
-        .status(400)
-        .json({ ok: false, msg: "There is already a subcategory with this name" });
+      return res.status(400).json({
+        ok: false,
+        msg: `There is already a subcategory ${name} in ${category}`,
+      });
     }
 
-    const categoryId = await Category.findOne({ name: category }).select("_id");
-    if (!categoryId) {
-      return res.status(400).json({ ok: false, msg: "There is no category with this name" });
-    }
+    const newCategory = new Subcategory({ name, category: searchCategory._id });
+    await newCategory.save();
 
-    let newSubcategory = new Subcategory({ name, category: categoryId._id });
-    newSubcategory = await newSubcategory.save();
-
-    res
-      .status(201)
-      .json({ ok: true, msg: `${newSubcategory.name} subcategory created successfully` });
+    res.status(201).json({
+      ok: true,
+      msg: `Subcategory called ${name} created in ${category} category successfully`,
+    });
   } catch (error) {
     internalServerError(error, res);
   }
 };
 
-export { getSubcategories, createSubcategory };
+const getSubcategories = async () => {
+  console.log("From getSubcategories controller");
+};
+
+export { createSubcategory, getSubcategories };
