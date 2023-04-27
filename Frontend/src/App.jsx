7ff9bd63e0440e6d687080233b98ac96bc7b2167
Nicolay Moreno <nicolay.mherrera@gmail.com>
2023-04-27 16:11:33 -0500

    Moved routes to RouterProvider.jsx component => (Frontend)

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,24 +1,8 @@
 // React-router-Dom
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
-// Auth
-import AuthLayout from "./layouts/AuthLayout";
-
-// Private Route
-import Shop from "./layouts/Shop";
-
-// Public Routes
-import LoginForm from "./components/public-components/LoginForm";
-import RegisterForm from "./components/public-components/RegisterForm";
-import ForgotPassword from "./components/public-components/ForgotPassword";
-import NewPassword from "./components/public-components/NewPassword";
-import ConfirmAccount from "./components/public-components/ConfirmAccount";
-
-// Private Routes
-import Home from "./components/private-components/Home";
-import Products from "./components/private-components/Categories";
-import ShopingCart from "./components/private-components/ShopingCart";
-import Settings from "./layouts/Settings";
+// Routes
+import RouterProvider from "./RouterProvider";
 
 // Context
 import { AuthProvider } from "./context/AuthProvider";
@@ -27,28 +11,7 @@ function App() {
   return (
     <BrowserRouter>
       <AuthProvider>
-        <Routes>
-          <Route path="/" element={<AuthLayout />}>
-            <Route index element={<LoginForm />} />
-            <Route path="register" element={<RegisterForm />} />
-            <Route path="confirm/:id" element={<ConfirmAccount />} />
-            <Route path="forgot-password" element={<ForgotPassword />} />
-            <Route path="forgot-password/:token" element={<NewPassword />} />
-            <Route path="*" element={<Navigate to="/" />} replace />
-          </Route>
-
-          <Route path="shop" element={<Shop />}>
-            <Route index element={<Home />} />
-            <Route path="categories" element={<Products />} />
-            <Route path="settings" element={<Settings />}>
-              <Route index element={<div>My shopping</div>} />
-              <Route path="update-data" element={<div>Update Data</div>} />
-              <Route path="manage-products" element={<div>Manage products</div>} />
-              <Route path="manage-subcategories" element={<div>Manage Subcategories</div>} />
-            </Route>
-            <Route path="shoping-cart" element={<ShopingCart />} />
-          </Route>
-        </Routes>
+        <RouterProvider />
       </AuthProvider>
     </BrowserRouter>
   );
---
+++ Frontend/src/RouterProvider.jsx
@@ -0,0 +1,50 @@
+// React-router-Dom
+import { Routes, Route, Navigate } from "react-router-dom";
+
+// Auth
+import AuthLayout from "./layouts/AuthLayout";
+
+// Private Route
+import Shop from "./layouts/Shop";
+
+// Public Routes
+import LoginForm from "./components/public-components/LoginForm";
+import RegisterForm from "./components/public-components/RegisterForm";
+import ForgotPassword from "./components/public-components/ForgotPassword";
+import NewPassword from "./components/public-components/NewPassword";
+import ConfirmAccount from "./components/public-components/ConfirmAccount";
+
+// Private Routes
+import Home from "./components/private-components/Home";
+import Products from "./components/private-components/Categories";
+import ShopingCart from "./components/private-components/ShopingCart";
+import Settings from "./layouts/Settings";
+
+const RouterProvider = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<AuthLayout />}>
+        <Route index element={<LoginForm />} />
+        <Route path="register" element={<RegisterForm />} />
+        <Route path="confirm/:id" element={<ConfirmAccount />} />
+        <Route path="forgot-password" element={<ForgotPassword />} />
+        <Route path="forgot-password/:token" element={<NewPassword />} />
+        <Route path="*" element={<Navigate to="/" />} replace />
+      </Route>
+
+      <Route path="shop" element={<Shop />}>
+        <Route index element={<Home />} />
+        <Route path="categories" element={<Products />} />
+        <Route path="settings" element={<Settings />}>
+          <Route index element={<div>My shopping</div>} />
+          <Route path="update-data" element={<div>Update Data</div>} />
+          <Route path="manage-products" element={<div>Manage products</div>} />
+          <Route path="manage-subcategories" element={<div>Manage Subcategories</div>} />
+        </Route>
+        <Route path="shoping-cart" element={<ShopingCart />} />
+      </Route>
+    </Routes>
+  );
+};
+
+export default RouterProvider;
