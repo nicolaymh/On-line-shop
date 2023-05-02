369eb34e0a03bc898b149e0482a5549e6e24e030
Nicolay Moreno <nicolay.mherrera@gmail.com>
2023-05-02 18:47:09 -0500

    designing sidebar

--- Frontend/src/layouts/Settings.jsx
+++ Frontend/src/layouts/Settings.jsx
@@ -7,19 +7,19 @@ const Settings = () => {
   const { auth } = useAuth();
 
   return (
-    <main className="mainContainer">
-      <div>
+    <div className="mainContainer">
+      <nav>
         <Link to="/shop/settings">My shopping</Link>
         <Link to="/shop/settings/edit-info">Edit Info</Link>
         <Link to="/shop/settings/add-products">Add Products</Link>
         <Link to="/shop/settings/manage-users">Manage Users</Link>
         <Link to="/shop/settings/manage-subcategories">Manage Subcategories</Link>
-      </div>
+      </nav>
 
-      <div>
+      <main>
         <Outlet />
-      </div>
-    </main>
+      </main>
+    </div>
   );
 };
 
