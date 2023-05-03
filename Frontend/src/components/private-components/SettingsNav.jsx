71c984092b8260309689e486e4dc56e021e9ecf7
Nicolay Moreno <nicolay.mherrera@gmail.com>
2023-05-02 20:49:56 -0500

    ***

--- Frontend/src/components/private-components/SettingsNav.jsx
+++ Frontend/src/components/private-components/SettingsNav.jsx
@@ -5,38 +5,42 @@ import style from "../../sass/settings/settingsNav.module.scss";
 
 const SettingsNav = () => {
   return (
-    <nav className={style.container}>
-      <NavLink
-        className={({ isActive }) => (isActive ? style.activeLink : null)}
-        to="/shop/settings"
-        end
-      >
-        My shopping
-      </NavLink>
-      <NavLink
-        className={({ isActive }) => (isActive ? style.activeLink : null)}
-        to="/shop/settings/edit-info"
-      >
-        Edit Info
-      </NavLink>
-      <NavLink
-        className={({ isActive }) => (isActive ? style.activeLink : null)}
-        to="/shop/settings/add-products"
-      >
-        Add Products
-      </NavLink>
-      <NavLink
-        className={({ isActive }) => (isActive ? style.activeLink : null)}
-        to="/shop/settings/manage-users"
-      >
-        Manage Users
-      </NavLink>
-      <NavLink
-        className={({ isActive }) => (isActive ? style.activeLink : null)}
-        to="/shop/settings/manage-subcategories"
-      >
-        Manage Subcategories
-      </NavLink>
+    <nav className={style.containerNav}>
+      <div className={style.containerLinks}>
+        <NavLink
+          className={({ isActive }) => (isActive ? style.activeLink : null)}
+          to="/shop/settings"
+          end
+        >
+          My shopping
+        </NavLink>
+        <NavLink
+          className={({ isActive }) => (isActive ? style.activeLink : null)}
+          to="/shop/settings/edit-info"
+        >
+          Edit Info
+        </NavLink>
+        <NavLink
+          className={({ isActive }) => (isActive ? style.activeLink : null)}
+          to="/shop/settings/add-products"
+        >
+          Add Products
+        </NavLink>
+        <NavLink
+          className={({ isActive }) => (isActive ? style.activeLink : null)}
+          to="/shop/settings/manage-users"
+        >
+          Manage Users
+        </NavLink>
+        <NavLink
+          className={({ isActive }) => (isActive ? style.activeLink : null)}
+          to="/shop/settings/manage-subcategories"
+        >
+          Manage Subcategories
+        </NavLink>
+      </div>
+
+      <div></div>
     </nav>
   );
 };
