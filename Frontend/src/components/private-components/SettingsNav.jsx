31f6142fa412d9e7ee14d61482fa2018d294de1c
Nicolay Moreno <nicolay.mherrera@gmail.com>
2023-05-03 20:09:37 -0500

    Added sidebar in SettingsNav.jsx => (Frontend)

--- Frontend/src/components/private-components/SettingsNav.jsx
+++ Frontend/src/components/private-components/SettingsNav.jsx
@@ -1,13 +1,22 @@
 import { NavLink } from "react-router-dom";
+import { useState } from "react";
 
 // CSS Styles ( SASS Modules )
 import style from "../../sass/settings/settingsNav.module.scss";
 
+// React-Icons
+import { FaAngleLeft } from "react-icons/fa";
+
 const SettingsNav = () => {
+  const [clicked, setClicked] = useState(false);
+
+  const openMenu = () => setClicked(!clicked);
+
   return (
     <nav className={style.containerNav}>
-      <div className={style.containerLinks}>
+      <div className={`${style.containerLinks} ${clicked ? style.open : ""}`}>
         <NavLink
+          onClick={openMenu}
           className={({ isActive }) => (isActive ? style.activeLink : null)}
           to="/shop/settings"
           end
@@ -15,24 +24,28 @@ const SettingsNav = () => {
           My shopping
         </NavLink>
         <NavLink
+          onClick={openMenu}
           className={({ isActive }) => (isActive ? style.activeLink : null)}
           to="/shop/settings/edit-info"
         >
           Edit Info
         </NavLink>
         <NavLink
+          onClick={openMenu}
           className={({ isActive }) => (isActive ? style.activeLink : null)}
           to="/shop/settings/add-products"
         >
           Add Products
         </NavLink>
         <NavLink
+          onClick={openMenu}
           className={({ isActive }) => (isActive ? style.activeLink : null)}
           to="/shop/settings/manage-users"
         >
           Manage Users
         </NavLink>
         <NavLink
+          onClick={openMenu}
           className={({ isActive }) => (isActive ? style.activeLink : null)}
           to="/shop/settings/manage-subcategories"
         >
@@ -40,7 +53,7 @@ const SettingsNav = () => {
         </NavLink>
       </div>
 
-      <div></div>
+      <FaAngleLeft onClick={openMenu} className={`${style.icon} ${clicked ? style.open : ""}`} />
     </nav>
   );
 };
