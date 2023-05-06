fd57c14f38894c36f047ad48b93dcfb9f0ec480d
Nicolay Moreno <nicolay.mherrera@gmail.com>
2023-05-05 21:36:21 -0500

    EditUserInfo.jsx: Ajusted container size with sass file ==> (frontend)

--- Frontend/src/components/private-components/settings/EditUserInfo.jsx
+++ Frontend/src/components/private-components/settings/EditUserInfo.jsx
@@ -21,152 +21,152 @@ import { Alert } from "../../general-components/Alert";
 import useAuth from "../../../Hooks/useAuth";
 
 const EditUserInfo = () => {
-  const { auth, setAuth } = useAuth();
+   const { auth, setAuth } = useAuth();
 
-  const { editUser: initialForm } = initialFormInputs();
+   const { editUser: initialForm } = initialFormInputs();
 
-  const [alert, setAlert] = useState({ msg: "", error: false });
+   const [alert, setAlert] = useState({ msg: "", error: false });
 
-  const { name, email, address, phone, password, onInputChange } = useForm({
-    ...initialForm,
-    name: auth.name,
-    email: auth.email,
-    address: auth.address,
-    phone: auth.phone,
-  });
+   const { name, email, address, phone, password, onInputChange } = useForm({
+      ...initialForm,
+      name: auth.name,
+      email: auth.email,
+      address: auth.address,
+      phone: auth.phone,
+   });
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+   const handleSubmit = async (e) => {
+      e.preventDefault();
 
-    // Form Validations
-    if ([name, email, address, phone, password].includes("")) {
-      return setAlert({ msg: "All fields are required", error: true });
-    }
+      // Form Validations
+      if ([name, email, address, phone, password].includes("")) {
+         return setAlert({ msg: "All fields are required", error: true });
+      }
 
-    if (password.length < 6) {
-      return setAlert({ msg: "The password must be at least 6 characters", error: true });
-    }
+      if (password.length < 6) {
+         return setAlert({ msg: "The password must be at least 6 characters", error: true });
+      }
 
-    if (address.length < 8) {
-      return setAlert({ msg: "Address is required and min 8 characters", error: true });
-    }
+      if (address.length < 8) {
+         return setAlert({ msg: "Address is required and min 8 characters", error: true });
+      }
 
-    if (phone.length < 10) {
-      return setAlert({ msg: "Phone is required and min 10 characters", error: true });
-    }
+      if (phone.length < 10) {
+         return setAlert({ msg: "Phone is required and min 10 characters", error: true });
+      }
 
-    try {
-      // API Call
-      const token = localStorage.getItem("token");
+      try {
+         // API Call
+         const token = localStorage.getItem("token");
 
-      const config = {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`,
-        },
-      };
+         const config = {
+            headers: {
+               "Content-Type": "application/json",
+               Authorization: `Bearer ${token}`,
+            },
+         };
 
-      const { data } = await axiosInstance.put(
-        "/users/edit-info",
-        { name, email, address, phone, password },
-        config
-      );
+         const { data } = await axiosInstance.put(
+            "/users/edit-info",
+            { name, email, address, phone, password },
+            config
+         );
 
-      setAuth(data.user);
+         setAuth(data.user);
 
-      setAlert({ msg: data.msg, error: false });
-    } catch (error) {
-      console.log(error);
+         setAlert({ msg: data.msg, error: false });
+      } catch (error) {
+         console.log(error);
 
-      const data = error.response.data.msg || error.response.data.errors[0].msg;
-      setAlert({ msg: data, error: true });
-    }
-  };
+         const data = error.response.data.msg || error.response.data.errors[0].msg;
+         setAlert({ msg: data, error: true });
+      }
+   };
 
-  return (
-    <div className={containerStyle.editUserContainer}>
-      <form onSubmit={handleSubmit} className={formStyle.form}>
-        {alert.msg && <Alert {...alert} />}
+   return (
+      <div className={containerStyle.editUserContainer}>
+         <form onSubmit={handleSubmit} className={formStyle.form}>
+            {alert.msg && <Alert {...alert} />}
 
-        <div className={formStyle.field}>
-          <label htmlFor="name">Name: </label>
+            <div className={formStyle.field}>
+               <label htmlFor="name">Name: </label>
 
-          <input
-            id="name"
-            name="name"
-            type="text"
-            placeholder="Full Name"
-            autoComplete="off"
-            value={name}
-            onChange={onInputChange}
-          />
-        </div>
+               <input
+                  id="name"
+                  name="name"
+                  type="text"
+                  placeholder="Full Name"
+                  autoComplete="off"
+                  value={name}
+                  onChange={onInputChange}
+               />
+            </div>
 
-        <div className={formStyle.field}>
-          <label htmlFor="email">E-mail: </label>
+            <div className={formStyle.field}>
+               <label htmlFor="email">E-mail: </label>
 
-          <input
-            id="email"
-            name="email"
-            type="email"
-            placeholder="Your e-mail"
-            autoComplete="off"
-            value={email}
-            onChange={onInputChange}
-          />
-        </div>
+               <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  placeholder="Your e-mail"
+                  autoComplete="off"
+                  value={email}
+                  onChange={onInputChange}
+               />
+            </div>
 
-        <div className={formStyle.field}>
-          <label htmlFor="address">Address: </label>
+            <div className={formStyle.field}>
+               <label htmlFor="address">Address: </label>
 
-          <input
-            id="address"
-            name="address"
-            type="text"
-            placeholder="Your Address"
-            autoComplete="off"
-            value={address}
-            onChange={onInputChange}
-          />
-        </div>
+               <input
+                  id="address"
+                  name="address"
+                  type="text"
+                  placeholder="Your Address"
+                  autoComplete="off"
+                  value={address}
+                  onChange={onInputChange}
+               />
+            </div>
 
-        <div className={formStyle.field}>
-          <label htmlFor="phone">Phone: </label>
+            <div className={formStyle.field}>
+               <label htmlFor="phone">Phone: </label>
 
-          <input
-            id="phone"
-            name="phone"
-            type="tel"
-            placeholder="Your Tel"
-            autoComplete="off"
-            value={phone}
-            onChange={onInputChange}
-          />
-        </div>
-        <div className={formStyle.field}>
-          <label htmlFor="password">Password: </label>
+               <input
+                  id="phone"
+                  name="phone"
+                  type="tel"
+                  placeholder="Your Tel"
+                  autoComplete="off"
+                  value={phone}
+                  onChange={onInputChange}
+               />
+            </div>
+            <div className={formStyle.field}>
+               <label htmlFor="password">Password: </label>
 
-          <input
-            id="password"
-            name="password"
-            type="password"
-            placeholder="password"
-            autoComplete="off"
-            value={password}
-            onChange={onInputChange}
-          />
-        </div>
+               <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  placeholder="password"
+                  autoComplete="off"
+                  value={password}
+                  onChange={onInputChange}
+               />
+            </div>
 
-        <div className={formStyle.field}>
-          <input type="submit" value="EDIT INFO" />
-        </div>
-      </form>
+            <div className={formStyle.field}>
+               <input type="submit" value="EDIT INFO" />
+            </div>
+         </form>
 
-      <div className={containerStyle.imgContainer}>
-        <img height="100%" src={registerImage} alt="register-image" />
+         <div className={containerStyle.imgContainer}>
+            <img height="100%" src={registerImage} alt="register-image" />
+         </div>
       </div>
-    </div>
-  );
+   );
 };
 
 export default EditUserInfo;
