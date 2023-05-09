94be273ef133b17b9ab599c0d6df08514edd512e
Nicolay Moreno <nicolay.mherrera@gmail.com>
2023-05-09 16:07:29 -0500

    Created renewToken function  ==> (Backend)

--- Backend/src/middlewares/renewToken.js
+++ Backend/src/middlewares/renewToken.js
@@ -1,18 +1,27 @@
 import jwt from "jsonwebtoken";
-import internalServerError from "../helpers/internalServerError.js";
+import generateJWT from "../helpers/tokens/generateJWT.js";
 
+/**
+ * This function renews a JWT token by verifying the existing token and generating a new one with a
+ * shorter expiration time.
+ * @param req - req stands for request and it is an object that contains information about the HTTP
+ * request that was made, such as the request method, headers, URL, and any data that was sent with the
+ * request. It is typically passed as the first parameter to an Express route handler function.
+ * @param res - `res` is the response object that is used to send a response back to the client making
+ * the request. It contains methods like `status` to set the HTTP status code of the response, `json`
+ * to send a JSON response, and many others.
+ */
 const renewToken = async (req, res) => {
    try {
-      const { token } = req.params;
+      let { token } = req.params;
 
-      const tokenDecode = jwt.verify(token, process.env.JWT_SECRET);
+      // Verify Token
+      const { id, name } = jwt.verify(token, process.env.JWT_SECRET);
 
-      console.log(tokenDecode);
+      // Generate a new token
+      token = await generateJWT(id, name, "30m");
 
-      const tokenCreated = tokenDecode.iat;
-      const tokenExpired = tokenDecode.exp;
-      console.log(new Date(tokenCreated * 1000));
-      console.log(tokenExpired);
+      res.status(201).json({ ok: true, token });
    } catch (error) {
       console.error(error);
 
