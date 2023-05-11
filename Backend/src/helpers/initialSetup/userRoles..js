8654a83ed7a8c8d98af4c9ca0768a59a7fd71161
Nicolay Moreno <nicolay.mherrera@gmail.com>
2023-05-11 11:43:21 -0500

    Refactoring createRoles function in userRoles.js ==> (backend)

--- Backend/src/helpers/initialSetup/userRoles..js
+++ Backend/src/helpers/initialSetup/userRoles..js
@@ -1,28 +1,25 @@
 import Role from "../../models/RoleModel.js";
 
-/**
- * The function creates three roles (user, moderator, and admin) if they do not already exist in the
- * database.
- * @returns If `countRoles` is greater than 0, nothing is returned. If `countRoles` is less than or
- * equal to 0, a Promise is returned that creates three new `Role` documents with the names "user",
- * "moderator", and "admin". If there is an error, it is logged to the console.
- */
+// Roles
+const roles = ["user", "moderator", "admin"];
+
 const createRoles = async () => {
-  try {
-    const countRoles = await Role.estimatedDocumentCount();
+   try {
+      const countRoles = await Role.estimatedDocumentCount();
 
-    if (countRoles > 0) return;
+      if (countRoles > 0) return;
 
-    await Promise.all([
-      new Role({ name: "user" }).save(),
-      new Role({ name: "moderator" }).save(),
-      new Role({ name: "admin" }).save(),
-    ]);
+      await Promise.all(
+         roles.map((role) => {
+            return new Role({ name: role }).save();
+         })
+      );
 
-    console.log("**********⭐⭐⭐ Created Roles: ⭐⭐⭐**********");
-  } catch (error) {
-    console.log(error);
-  }
+      console.log("**********⭐⭐⭐ Created Roles: ⭐⭐⭐**********");
+   } catch (error) {
+      console.error(`Error creating roles: ${error.message}`);
+      throw error;
+   }
 };
 
 export default createRoles;
