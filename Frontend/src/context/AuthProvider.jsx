18ad48571370b2447815b1824f26bc890e720085
Nicolay Moreno <nicolay.mherrera@gmail.com>
2023-05-11 16:11:29 -0500

    Creating Category Context ==> (Frontend)

--- Frontend/src/context/AuthProvider.jsx
+++ Frontend/src/context/AuthProvider.jsx
@@ -1,5 +1,7 @@
-import { useEffect, useState } from "react";
 import { createContext } from "react";
+
+import { useEffect, useState } from "react";
+
 import { useNavigate } from "react-router-dom";
 
 import axiosInstance from "../helpers/axiosInstance";
---
+++ Frontend/src/context/CategoryProvider.jsx
@@ -0,0 +1,8 @@
+import { createContext } from "react";
+
+// Context
+const CategoryContext = createContext();
+
+const CategoryProvider = () => {};
+
+export default CategoriesProvider;
