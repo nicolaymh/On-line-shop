f49d99a2c309132d179028c56f219bc6a14196cc
Nicolay Moreno <nicolay.mherrera@gmail.com>
2023-05-11 13:30:08 -0500

    +++

--- Backend/src/helpers/initialSetup/userRoles..js
+++ Backend/src/helpers/initialSetup/userRoles..js
@@ -5,9 +5,10 @@ const roles = ["user", "moderator", "admin"];
 
 /**
  * This function creates roles in a database if they do not already exist.
- * @returns If `countRoles` is greater than 0, the function will return nothing (`undefined`). If
- * `countRoles` is less than or equal to 0, the function will return a Promise that resolves when all
- * the roles in the `roles` array have been created and saved to the database.
+ * @returns If the `countRoles` variable is greater than 0, the function will return nothing. If the
+ * `countRoles` variable is less than or equal to 0, the function will return a Promise that resolves
+ * when all the roles in the `roles` array have been created and saved to the database. If there is an
+ * error during the process, the function will throw an error.
  */
 const createRoles = async () => {
    try {
