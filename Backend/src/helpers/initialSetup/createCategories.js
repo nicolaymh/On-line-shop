44cef5166f8da077ced0984f80bcd65664f16ba6
Nicolay Moreno <nicolay.mherrera@gmail.com>
2023-05-11 13:07:59 -0500

    Added trycatch in createCategories function in createCategories.js ==> (backend)

--- Backend/src/helpers/initialSetup/createCategories.js
+++ Backend/src/helpers/initialSetup/createCategories.js
@@ -2,34 +2,36 @@ import Category from "../../models/CatagoryModel.js";
 
 // Categories initial Setup
 let categories = [
-  { name: "playstation", description: "sony entertainment", CreateInDb: "" },
-  { name: "xbox", description: "microsoft", CreateInDb: "" },
-  { name: "nintendo", description: "nintendo", CreateInDb: "" },
-  { name: "laptops", description: "laptop brands", CreateInDb: "" },
+   { name: "playstation", description: "sony entertainment", CreateInDb: "" },
+   { name: "xbox", description: "microsoft", CreateInDb: "" },
+   { name: "nintendo", description: "nintendo", CreateInDb: "" },
+   { name: "laptops", description: "laptop brands", CreateInDb: "" },
 ];
 
-/**
- * This function creates new categories in a database and checks if they already exist.
- */
 const createCategories = async () => {
-  const newCategory = await Promise.all(
-    categories.map(async (c) => {
-      const { name, description } = c;
+   try {
+      const newCategory = await Promise.all(
+         categories.map(async (c) => {
+            const { name, description } = c;
 
-      const exists = await Category.findOne({ name });
+            const exists = await Category.findOne({ name });
 
-      if (exists) return { ...c, CreateInDb: "It already exists in the DB 🔴" };
+            if (exists) return { ...c, CreateInDb: "It already exists in the DB 🔴" };
 
-      if (!exists) {
-        new Category({ name, description }).save();
+            if (!exists) {
+               new Category({ name, description }).save();
 
-        return { ...c, CreateInDb: "Created in the DB 🟢" };
-      }
-    })
-  );
+               return { ...c, CreateInDb: "Created in the DB 🟢" };
+            }
+         })
+      );
 
-  console.log("**********⭐⭐⭐ Categories in the DB: ⭐⭐⭐**********");
-  console.log(newCategory);
+      console.log("**********⭐⭐⭐ Categories in the DB: ⭐⭐⭐**********");
+      console.log(newCategory);
+   } catch (error) {
+      console.error(`Error creating categories: ${error.message}`);
+      throw error;
+   }
 };
 
 export default createCategories;
