d6b319cc82fe43bc5e822dde22d7aaf82dfe8eb0
Nicolay Moreno <nicolay.mherrera@gmail.com>
2023-05-13 13:30:06 -0500

    Creating manageUser controller in manage.controller.js ==> (Backend)

--- Backend/src/routes/manage.router.js
+++ Backend/src/routes/manage.router.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
-
+import checkAuth from "../middlewares/checkAuth.js";
 import * as manage from "../controllers/manage.controller.js";
 
 const router = Router();
 
-router.get("/user", manage.manageUser);
+router.get("/user", checkAuth, manage.manageUser);
 
 export default router;
