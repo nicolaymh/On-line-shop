6d2158c4cabbd59e2c945f9853ea919cf38ccc72
Nicolay Moreno <nicolay.mherrera@gmail.com>
2023-05-14 16:05:10 -0500

    Creating manageUser controller in manage.controller.js ==> (Backend) +++

--- Backend/src/controllers/manage.controller.js
+++ Backend/src/controllers/manage.controller.js
@@ -1,6 +1,16 @@
-const manageUser = (req, res) => {
-   console.log("From user manage");
-   console.log(req.user);
+import Role from "../models/RoleModel.js";
+
+const manageUser = async (req, res) => {
+   try {
+      const { user } = req;
+      const { email } = req.params;
+
+      const roles = await Role.find({}).nor({ name: "admin" }).lean().select("_id, name");
+
+      if (user.role !== "admin") {
+         return res.status(400).json({ ok: false, msg: "Access denied for this request." });
+      }
+   } catch (error) {}
 };
 
 export { manageUser };
--- Backend/src/routes/manage.router.js
+++ Backend/src/routes/manage.router.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
+import { forgetPassword as validateEmail } from "../middlewares/validate/validationsUser/validate-user.js";
 import checkAuth from "../middlewares/auth/checkAuth.js";
 import * as manage from "../controllers/manage.controller.js";
 
 const router = Router();
 
-router.get("/user", checkAuth, manage.manageUser);
+router.get("/user/:email", validateEmail, checkAuth, manage.manageUser);
 
 export default router;
