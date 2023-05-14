c279d8812ec3d376f7e42b3aca515e527e468090
Nicolay Moreno <nicolay.mherrera@gmail.com>
2023-05-14 17:41:11 -0500

    Added manageUser function in manage.controller.js ==> (backend)

--- Backend/src/controllers/manage.controller.js
+++ Backend/src/controllers/manage.controller.js
@@ -1,16 +1,43 @@
 import Role from "../models/RoleModel.js";
+import User from "../models/UserModel.js";
+
+import internalServerError from "../helpers/internalServerError.js";
 
 const manageUser = async (req, res) => {
    try {
-      const { user } = req;
+      // Info admin
+      const { user: admin } = req;
+      if (admin.role !== "admin") return console.log("You cannot change permissions.");
+
+      // Info => user email to change permission.
       const { email } = req.params;
 
-      const roles = await Role.find({}).nor({ name: "admin" }).lean().select("_id, name");
+      // Find user to modify permission.
+      const userInfo = await User.findOne({ email })
+         .populate("role", "name")
+         .select("_id name email address phone role")
+         .lean();
 
-      if (user.role !== "admin") {
-         return res.status(400).json({ ok: false, msg: "Access denied for this request." });
+      // you can not change the permissions for the admin.
+      if (userInfo.role.name === "admin") {
+         return res.status(400).json({
+            ok: false,
+            msg: "you cannot change the permissions for this user.",
+         });
       }
-   } catch (error) {}
+
+      // Find all the roles that a user can have, except the administrator role.
+      const roles = await Role.find().nor({ name: "admin" }).lean().select("_id, name");
+
+      // Response ==> request ok.
+      res.status(201).json({
+         ok: true,
+         userInfo: { ...userInfo, role: userInfo.role.name },
+         roleOptions: roles,
+      });
+   } catch (error) {
+      internalServerError(error, res);
+   }
 };
 
 export { manageUser };
