08fa860b81479dbe023522bab5c0c7e439faf411
Nicolay Moreno <nicolay.mherrera@gmail.com>
2023-05-15 15:10:21 -0500

    Starting to design manage-user route from settings +++

--- Frontend/src/components/private-components/settings/ManageUsers.jsx
+++ Frontend/src/components/private-components/settings/ManageUsers.jsx
@@ -1,9 +1,17 @@
-import React from "react";
+import style from "../../../sass/forms/userPermissions.module.scss";
 
 const ManageUsers = () => {
    console.log("From ManageUsers");
 
-   return <section>ManageUsers</section>;
+   return (
+      <section className={style.sectionContainer}>
+         <h2>Change user permissions</h2>
+
+         <form>
+            <input type="text" placeholder="Search user" />
+         </form>
+      </section>
+   );
 };
 
 export default ManageUsers;
