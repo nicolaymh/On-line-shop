e3191e4a4d34d624cade30ef857d4a4e0ad8d62f
Nicolay Moreno <nicolay.mherrera@gmail.com>
2023-05-15 16:14:23 -0500

    Starting to design manage-user route from settings +++

--- Frontend/src/components/private-components/settings/ManageUsers.jsx
+++ Frontend/src/components/private-components/settings/ManageUsers.jsx
@@ -1,29 +1,10 @@
 // CSS Styles ( SASS Modules )
 import style from "../../../sass/forms/userPermissions.module.scss";
-import formStyle from "../../../sass/forms/generalFormStyle.module.scss";
 
 const ManageUsers = () => {
    console.log("From ManageUsers");
 
-   return (
-      <section className={style.sectionContainer}>
-         <div className={style.searchUser}>
-            <h2>Change user permissions</h2>
-
-            <form className={formStyle.form}>
-               <div className={formStyle.field}>
-                  <label htmlFor="">Email:</label>
-
-                  <input type="text" placeholder="Search user" />
-               </div>
-
-               <div className={formStyle.field}>
-                  <input type="submit" value="LOG IN" />
-               </div>
-            </form>
-         </div>
-      </section>
-   );
+   return <section></section>;
 };
 
 export default ManageUsers;
