50fbc4cf4aede9806cc1ec8735be1654be7735d5
Nicolay Moreno <nicolay.mherrera@gmail.com>
2023-05-15 18:22:28 -0500

    ManageUser.jsx: Added form to find user ==> (frontend)

--- Frontend/src/components/private-components/settings/ManageUsers.jsx
+++ Frontend/src/components/private-components/settings/ManageUsers.jsx
@@ -1,10 +1,26 @@
 // CSS Styles ( SASS Modules )
 import style from "../../../sass/forms/userPermissions.module.scss";
+import formStyle from "../../../sass/forms/formInputs.module.scss";
 
 const ManageUsers = () => {
    console.log("From ManageUsers");
 
-   return <section></section>;
+   return (
+      <section className={style.sectionContainer}>
+         <h3>Modify user Permission</h3>
+
+         <form>
+            <div className={formStyle.field}>
+               <label htmlFor="">Email: </label>
+               <input type="email" placeholder="Search User" />
+            </div>
+
+            <div className={formStyle.field}>
+               <input type="submit" value="Find User" />
+            </div>
+         </form>
+      </section>
+   );
 };
 
 export default ManageUsers;
