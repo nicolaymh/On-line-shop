06c44a3a7aa448668d5e31b9bf4181bcad186d9f
Nicolay Moreno <nicolay.mherrera@gmail.com>
2023-05-16 20:52:29 -0500

    UserPermissionInfo.jsx: Adding custom checkbox ==> (frontend)

--- Frontend/src/components/private-components/settings/UserPermissionInfo.jsx
+++ Frontend/src/components/private-components/settings/UserPermissionInfo.jsx
@@ -3,7 +3,9 @@ import inputStyles from "../../../sass/forms/formInputs.module.scss";
 import formStyle from "../../../sass/settings/userPermissionInfo.module.scss";
 
 const UserPermissionInfo = ({ userData }, { roleOptions }) => {
-   const { name, email, address, phone, role } = userData;
+   const { _id, name, email, address, phone, role } = userData;
+
+   console.log(roleOptions);
 
    return (
       <form className={formStyle.form}>
@@ -12,30 +14,36 @@ const UserPermissionInfo = ({ userData }, { roleOptions }) => {
          <div>
             <div className={inputStyles.field}>
                <label htmlFor="name">Name: </label>
-               <input id="name" name="name" type="text" value={name} />
+               <input id="name" name="name" type="text" value={name} readOnly />
             </div>
             <div className={inputStyles.field}>
                <label htmlFor="email">Email: </label>
-               <input id="email" name="email" type="email" value={email} />
+               <input id="email" name="email" type="email" value={email} readOnly />
             </div>
             <div className={inputStyles.field}>
                <label htmlFor="address">Address: </label>
-               <input id="address" name="address" type="text" value={address} />
+               <input id="address" name="address" type="text" value={address} readOnly />
             </div>
             <div className={inputStyles.field}>
                <label htmlFor="phone">phone: </label>
-               <input id="phone" name="phone" type="text" value={phone} />
+               <input id="phone" name="phone" type="text" value={phone} readOnly />
             </div>
 
-            <div>
-               <div>
+            <div className={formStyle.checkboxesContainer}>
+               <div className={formStyle.checkboxField}>
                   <label htmlFor="user">User: </label>
-                  <input id="user" type="checkbox" />
+
+                  <div className={formStyle.checkboxContainer}>
+                     <div className={formStyle.myCheckboxInactive}></div>
+                  </div>
                </div>
 
-               <div>
+               <div className={formStyle.checkboxField}>
                   <label htmlFor="moderator">Moderator: </label>
-                  <input id="moderator" type="checkbox" />
+
+                  <div className={formStyle.checkboxContainer}>
+                     <div className={formStyle.myCheckboxActive}></div>
+                  </div>
                </div>
             </div>
 
