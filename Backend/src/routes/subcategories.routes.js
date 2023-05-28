5f62d0bbdd0daa0b8d5371b9502f64e00b88e0d8
Nicolay Moreno <nicolay.mherrera@gmail.com>
2023-05-28 15:26:28 -0500

    Creating createCategory controller ==> (backend)

--- Backend/src/routes/subcategories.routes.js
+++ Backend/src/routes/subcategories.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 
 import * as subcategoriesController from "../controllers/subcategories.controller.js";
+import checkAuth from "../middlewares/auth/checkAuth.js";
 
 const router = Router();
 
 router.get("/get-subcategories", subcategoriesController.getSubcategories);
-router.post("/create-subcategory/:category", subcategoriesController.createSubcategory);
+router.post("/create-subcategory/:category", checkAuth, subcategoriesController.createSubcategory);
 
 export default router;
