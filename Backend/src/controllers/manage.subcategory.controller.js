29a843c8e24394e173fedb8334fd488b66f9ce91
Nicolay Moreno <nicolay.mherrera@gmail.com>
2023-05-28 17:37:50 -0500

    Creating createCategory controller ==> (backend)

--- Backend/src/controllers/manage.subcategory.controller.js
+++ Backend/src/controllers/manage.subcategory.controller.js
@@ -1,5 +1,14 @@
-const createSubcategory = (req, res) => {
+import User from "../models/UserModel.js";
 
+const createSubcategory = async (req, res) => {
+   const _id = req.user._id;
+
+   const isAdmin = await User.findById({ _id }).populate("role").select("role");
+   if (isAdmin.role.name !== "admin") {
+      return res.status(400).json({ ok: false, msg: "Access denied" });
+   }
+
+   console.log("Hey");
 };
 
 export { createSubcategory };
