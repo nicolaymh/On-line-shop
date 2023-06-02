5ce820ed81fb46fe075d300039be2e325a6bc2ea
Nicolay Moreno <nicolay.mherrera@gmail.com>
2023-06-01 20:20:14 -0500

    Creating reusable SelectOptions.jsx component ==> (Frontned)

---
+++ Frontend/src/components/general-components/SelectOptions.jsx
@@ -0,0 +1,16 @@
+const SelectOptions = ({ handleSelected, arrayOptions, infoTitle }) => {
+   return (
+      <>
+         <select onChange={handleSelected}>
+            <option value="-1">{infoTitle}</option>
+            {arrayOptions?.map(({ _id, name }) => (
+               <option key={_id} value={_id}>
+                  {name.toUpperCase()}
+               </option>
+            ))}
+         </select>
+      </>
+   );
+};
+
+export default SelectOptions;
--- Frontend/src/components/private-components/settings/ManageSubcategories.jsx
+++ Frontend/src/components/private-components/settings/ManageSubcategories.jsx
@@ -1,13 +1,45 @@
+import { useState } from "react";
+
 import useCategory from "../../../Hooks/useCategory";
 
 import style from "../../../sass/settings/manageSubcategories.module.scss";
+import SelectOptions from "../../general-components/SelectOptions";
 
 const ManageSubcategories = () => {
+   const [category, setCategory] = useState({});
+   const [subcategories, setSubcategories] = useState([]);
+
    const { categoryinfoAll } = useCategory();
 
    console.log(categoryinfoAll);
 
-   return <section className={style.manageSubcategoriesContainer}>ManageSubcategories</section>;
+   const handleSelected = ({ target }) => {
+      if (target.value === "-1") {
+         setCategory({});
+         setSubcategories([]);
+         return;
+      }
+
+      const categoryInfo = categoryinfoAll.filter(({ _id }) => _id === target.value);
+      const { _id, name, subcategories } = categoryInfo[0];
+
+      setCategory({ _id, name });
+      setSubcategories([...subcategories]);
+   };
+
+   return (
+      <section className={style.manageSubcategoriesContainer}>
+         <h4>Manage Subcategories</h4>
+
+         <div>
+            <SelectOptions
+               handleSelected={handleSelected}
+               arrayOptions={categoryinfoAll}
+               infoTitle="--Select-Category--"
+            />
+         </div>
+      </section>
+   );
 };
 
 export default ManageSubcategories;
