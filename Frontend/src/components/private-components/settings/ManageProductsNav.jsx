f4a9f936fb03f103502a3267d908009f2a5fa902
Nicolay Moreno <nicolay.mherrera@gmail.com>
2023-06-23 21:13:46 -0500

    Creating ManageProductNav.jsx ==> ( frontend )

--- Frontend/src/components/private-components/settings/ManageProductsNav.jsx
+++ Frontend/src/components/private-components/settings/ManageProductsNav.jsx
@@ -1,9 +1,12 @@
+// CSS Styles ( SASS Modules ).
+import style from "../../../sass/settings/manageProductsNav.module.scss";
+
 import { NavLink } from "react-router-dom";
 
 const ManageProductsNav = () => {
    return (
-      <nav>
-         <div>
+      <nav className={style.navContainer}>
+         <div className={style.linksContainer}>
             <NavLink to="/shop/settings/manage-products">Add Product</NavLink>
             <NavLink to="/shop/settings/manage-products/manage-product">Manage Product</NavLink>
          </div>
