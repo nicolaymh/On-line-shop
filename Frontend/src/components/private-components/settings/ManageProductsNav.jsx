43de649fdc5c0b260903135d7d29281a2063e2d3
Nicolay Moreno <nicolay.mherrera@gmail.com>
2023-06-26 14:23:03 -0500

    Creating ManageProductNav.jsx ==> ( frontend ) +

--- Frontend/src/components/private-components/settings/ManageProductsNav.jsx
+++ Frontend/src/components/private-components/settings/ManageProductsNav.jsx
@@ -1,14 +1,33 @@
 // CSS Styles ( SASS Modules ).
 import style from "../../../sass/settings/manageProductsNav.module.scss";
 
+// React-Icons Library.
+import { SiAddthis } from "react-icons/Si";
+import { AiFillEdit } from "react-icons/Ai";
+
+// React-Router-Dom.
 import { NavLink } from "react-router-dom";
 
 const ManageProductsNav = () => {
    return (
       <nav className={style.navContainer}>
+         <h3>Manage Products</h3>
+
          <div className={style.linksContainer}>
-            <NavLink to="/shop/settings/manage-products">Add Product</NavLink>
-            <NavLink to="/shop/settings/manage-products/manage-product">Manage Product</NavLink>
+            <div className={style.LinkContainer}>
+               <NavLink className={style.Link} to="/shop/settings/manage-products">
+                  Add Product
+               </NavLink>
+
+               <SiAddthis className={style.icon} />
+            </div>
+            <div className={style.LinkContainer}>
+               <NavLink className={style.Link} to="/shop/settings/manage-products/manage-product">
+                  Manage Product
+               </NavLink>
+
+               <AiFillEdit className={style.icon} />
+            </div>
          </div>
       </nav>
    );
