7efa191bfd276335bc607e0b3ad83ac9afd229c4
Nicolay Moreno <nicolay.mherrera@gmail.com>
2023-07-14 21:50:06 -0500

    Creating ManageProduct.jsx component ==> ( frontend )

--- Frontend/src/components/private-components/settings/ManageProduct.jsx
+++ Frontend/src/components/private-components/settings/ManageProduct.jsx
@@ -1,4 +1,10 @@
+import useProducts from "../../../Hooks/useProducts";
+
 const ManageProduct = () => {
+   const { ProductsInfo, setProductsInfo } = useProducts();
+
+   console.log(ProductsInfo);
+
    return <div>ManageProduct</div>;
 };
 
