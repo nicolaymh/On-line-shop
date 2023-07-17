219be7244510a3d4a7b02ed91863d383f1e8be46
Nicolay Moreno <nicolay.mherrera@gmail.com>
2023-07-17 13:43:25 -0500

    Creating products table in ManageProduct.jsx

--- Frontend/src/components/private-components/settings/ManageProduct.jsx
+++ Frontend/src/components/private-components/settings/ManageProduct.jsx
@@ -1,11 +1,41 @@
+// Context
+import useCategory from "../../../Hooks/useCategory";
 import useProducts from "../../../Hooks/useProducts";
 
 const ManageProduct = () => {
    const { ProductsInfo, setProductsInfo } = useProducts();
+   const { categoryinfoAll } = useCategory();
 
    console.log(ProductsInfo);
+   console.log(categoryinfoAll);
 
-   return <div>ManageProduct</div>;
+   return (
+      <div>
+         <table>
+            <thead>
+               <tr>
+                  <th>Name</th>
+                  <th>description</th>
+                  <th>Price</th>
+                  <th>Status</th>
+               </tr>
+            </thead>
+
+            <tbody>
+               {ProductsInfo.map((product) => {
+                  return (
+                     <tr key={product._id}>
+                        <td>{product.name}</td>
+                        <td>{product.description}</td>
+                        <td>{product.price}</td>
+                        <td>{product.status ? "Active" : "Inactive"}</td>
+                     </tr>
+                  );
+               })}
+            </tbody>
+         </table>
+      </div>
+   );
 };
 
 export default ManageProduct;
