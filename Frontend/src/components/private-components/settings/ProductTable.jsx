64944e89e122b32caee570a759d1f0f9fdea75b2
Nicolay Moreno <nicolay.mherrera@gmail.com>
2023-07-17 16:49:47 -0500

    Making ProductTable.jsx ==> (frontend)

--- Frontend/src/components/private-components/settings/ProductTable.jsx
+++ Frontend/src/components/private-components/settings/ProductTable.jsx
@@ -1,23 +1,31 @@
+// CSS Styles ( SASS Modules ).
+import style from "../../../sass/settings/tableProduct.module.scss";
+
 const ProductTable = ({ ProductsInfo }) => {
    return (
-      <div>
-         <table>
+      <div className={style.containerTable}>
+         <table className={style.table}>
             <thead>
                <tr>
                   <th>Name</th>
-                  <th>description</th>
-                  <th>Price</th>
+                  <th className={style.tDescription}>Description</th>
+                  <th className={style.tPrice}>Price</th>
                   <th>Status</th>
                </tr>
             </thead>
 
             <tbody>
-               {ProductsInfo.map((product) => {
+               {ProductsInfo.slice(0, 6).map((product) => {
                   return (
                      <tr key={product._id}>
                         <td>{product.name}</td>
-                        <td>{product.description}</td>
-                        <td>{product.price}</td>
+                        <td className={style.tDescription}>{product.description}</td>
+                        <td className={style.tPrice}>
+                           {product.price.toLocaleString("es-CO", {
+                              style: "currency",
+                              currency: "COP",
+                           })}
+                        </td>
                         <td>{product.status ? "Active" : "Inactive"}</td>
                      </tr>
                   );
