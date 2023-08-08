915c9f2626683d7c27ad35d57f72989871f5f447
Nicolay Moreno <nicolay.mherrera@gmail.com>
2023-08-08 18:52:48 -0500

    ShoppingCartProvider.jsx: Adding styles to price container ==> ( Frontend )

--- Frontend/src/components/private-components/shoppingCart/ShoppingCart.jsx
+++ Frontend/src/components/private-components/shoppingCart/ShoppingCart.jsx
@@ -22,11 +22,22 @@ const ShoppingCart = () => {
 
    return (
       <main className={style.mainContainer}>
-         <section>
-            <div>
+         <section className={style.sectionProducts}></section>
+         <section className={style.sectionPrices}>
+            <div className={style.containerPrices}>
                <p>
-                  Total: <span>{total}</span>
+                  Total price: <span>{total}</span>
                </p>
+               <p>
+                  TAX: <span>1234</span>
+               </p>
+               <p>
+                  Total: <span>123456</span>
+               </p>
+            </div>
+
+            <div className={style.containerButton}>
+               <button>Make Purchase</button>
             </div>
          </section>
       </main>
