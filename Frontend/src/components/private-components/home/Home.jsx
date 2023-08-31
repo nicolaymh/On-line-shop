874f1c96781a27e6b077f1d2d2ffaa693bae7628
Nicolay Moreno <nicolay.mherrera@gmail.com>
2023-08-31 17:53:07 -0500

    Starting with Home route ==> ( Frontend ) ++

--- Frontend/src/components/private-components/home/Home.jsx
+++ Frontend/src/components/private-components/home/Home.jsx
@@ -1,5 +1,44 @@
+// CSS Styles ( SASS Modules ).
+import style from "../../../sass/home/home.module.scss";
+
+// React-Hooks.
+import { useEffect, useState } from "react";
+
+// Context.
+import useProducts from "../../../Hooks/useProducts";
+
+// Components.
+import GenericComponents from "../../generic-components";
+
 const Home = () => {
-   return <></>;
+   const [loader, setLoader] = useState(false);
+
+   const { ProductsInfo } = useProducts();
+
+   useEffect(() => {
+      if (ProductsInfo.length > 0) {
+         const timer = setTimeout(() => {
+            console.log(ProductsInfo);
+            setLoader(true);
+         }, 500);
+
+         return () => {
+            clearTimeout(timer);
+         };
+      }
+   }, [ProductsInfo]);
+
+   return (
+      <>
+         {!loader ? (
+            <GenericComponents.Loader />
+         ) : (
+            <div className={style.container}>
+               <img src="" alt="carousel-img" />
+            </div>
+         )}
+      </>
+   );
 };
 
 export default Home;
