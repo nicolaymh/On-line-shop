c8d5240faa5cae23f658dcc2b679e2ed21827147
Nicolay Moreno <nicolay.mherrera@gmail.com>
2023-09-04 20:38:44 -0500

    Footer.jsx: Finished coponente ==> { Frontend }

--- Frontend/src/components/private-components/home/Footer.jsx
+++ Frontend/src/components/private-components/home/Footer.jsx
@@ -1,5 +1,57 @@
+// CSS Styles ( SASS Modules ).
+import style from "../../../sass/home/home.module.scss";
+
+// React-Icons.
+import {
+   SiNintendo,
+   SiPlaystation,
+   SiXbox,
+   SiSega,
+   SiAsus,
+   SiNvidia,
+   SiMicrosoft,
+   SiApple,
+   SiIntel,
+} from "react-icons/si";
+
 const Footer = () => {
-   return <div>Footer</div>;
+   return (
+      <div className={style.footerContainer}>
+         <div className={style.gridContainer}>
+            <div className={style.item}>
+               <h4>About Us</h4>
+               <p>
+                  We are an ecommerce dedicated to sell things about video games like consoles,
+                  video games, retro things, devices and accessories. Our service is based by online
+                  shopping and we send your product home.
+               </p>
+            </div>
+            <div className={style.item}>
+               <h4>Let Us Help</h4>
+               <p>About Us</p>
+               <p>Extra Services</p>
+               <p>Contact Us</p>
+               <p>Retro</p>
+            </div>
+            <div className={style.item}>
+               <h4>Some of our brands</h4>
+
+               <div className={style.brandsContainer}>
+                  <SiNintendo className={style.icon} />
+                  <SiPlaystation className={style.icon} />
+                  <SiXbox className={style.icon} />
+                  <SiSega className={style.icon} />
+                  <SiIntel className={style.icon} />
+                  <SiNvidia className={style.icon} />
+                  <SiMicrosoft className={style.icon} />
+                  <SiApple className={style.icon} />
+               </div>
+            </div>
+         </div>
+
+         <p>Copyright ©2022 All rights reserved | This Website is Developed by Nicolay Moreno</p>
+      </div>
+   );
 };
 
 export default Footer;
